4da7ec6f9bb6ad23f880304ba826c9ac89675736
phongbv <phongbv901@gmail.com>
2017-05-14 15:26:39 +0700

    add proj

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,3 +1,12 @@
+import {Component} from '@angular/core';
 /**
  * Created by phongbv on 5/14/17.
  */
+
+@Component ({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
